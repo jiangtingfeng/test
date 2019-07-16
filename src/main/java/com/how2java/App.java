f5479a2994d4e5536b56ee14928e0716f1511414
jiangtingfeng <2315507963@qq.com>
2019-07-16 18:54:38 +0800

    test

--- src/main/java/com/how2java/App.java
+++ src/main/java/com/how2java/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args ) throws Exception
     {
         Class.forName("com.mysql.jdbc.Driver");
-        System.out.println( "Hello World!" );
+        System.out.println( "Hell谁说的o World!" );
     }
 }
